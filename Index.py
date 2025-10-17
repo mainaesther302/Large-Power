0ac4ef5b649fced64904087f888f901be08ffbb3
mainaesther302 <mainaesther302@gmail.com>
2025-10-17 16:45:26 +0300

    discount checker

--- Index.py
+++ Index.py
@@ -1,3 +1,32 @@
+# Discount
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        final_price = price - (price * discount_percent / 100)
+        return final_price
+    else:
+        return price
+
+# Getting user input
+price = float(input("Enter the original price: "))
+discount = float(input("Enter the discount percentage: "))
+
+# Calling function and printing result
+final_price = calculate_discount(price, discount)
+
+if final_price != price:
+    print(f"Discount applied! Final price is: {final_price}")
+else:
+    print(f"No discount applied. Final price is: {price}")
+
+
+
+
+
+
+
+
+
+
 # Function with parameters
 def large_power(base, exponent):
     if base ** exponent > 5000:
